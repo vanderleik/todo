6505d5d9f6c1933a6e0ea934054517c09555e013
vanderleik <vanderleik@yahoo.com.br>
2023-07-08 09:26:07 -0300

    Criando retorno no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
 import { useState } from 'react'
 
 function App() {
-  return <div>Olá React!</div>
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+  return (
+    <div className='app'>
+      <h1>Lista de tarefas</h1>
+      <div className='todo-list'>
+        {
+          todos.map((todo) => (
+            <div className="todo">
+              <div className="content">
+                <p>{todo.text}</p>
+                <p className='category'>({todo.category})</p>
+              </div>
+            <div>
+              <button>Completar</button>
+              <button>x</button>
+            </div>
+            </div>
+          ))}
+    </div>
+    </div>
+    );
 }
 
 export default App
