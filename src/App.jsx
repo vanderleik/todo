86dc17b1af091fcaab1d4c5b18bccadceb4d9900
vanderleik <vanderleik@yahoo.com.br>
2023-07-08 16:38:35 -0300

    Adicionando funcionalidade ao filtro

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,8 @@ function App() {
   ]);
 
   const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [...todos,{
@@ -59,10 +61,13 @@ function App() {
     <div className='app'>
       <h1>Lista de tarefas</h1>
       <Search search={search} setSearch={setSearch}/>
-      <Filter />
+      <Filter filter={filter} setFilter={setFilter}/>
       <div className='todo-list'>
         {
-          todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
+          todos
+          .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
+          .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+          .map((todo) => (
             <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
             ))}
     </div>
